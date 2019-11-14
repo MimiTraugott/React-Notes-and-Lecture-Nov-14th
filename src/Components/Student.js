ffd985be4dd0ea019a100870a69277fd9f9a40fd
Mimi <mimitraugott@gmail.com>
2019-11-14 13:17:25 -0700

    afternoon mini project

---
+++ src/Components/Student.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Student extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            questionsAnswereed: 0
+        }
+    }
+}
